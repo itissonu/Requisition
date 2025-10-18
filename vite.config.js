2f66817759adcf855766a65c2ebe4abb0d0c3c4d
itissonu <srssoumyaranjansahu123@gmail.com>
2025-10-18 17:54:52 +0530

    transport git added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
-});
-   
+  plugins: [react(),tailwindcss(),],
+    server: {
+    historyApiFallback: true,
+  },
+})
